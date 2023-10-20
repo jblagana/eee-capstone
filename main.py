2c0c52f674368142c367a8c6d5ce4d29ba2221c9
jblagana <jan.rhey.lagana@eee.upd.edu.ph>
2023-10-20 23:04:45 +0800

    Removed download_unzip function and unnecessary imports

--- main.py
+++ main.py
@@ -2,29 +2,6 @@
 import os
 import sys
 import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
-
-from zipfile import ZipFile
-from urllib.request import urlretrieve
-
-def download_and_unzip(url, save_path):
-    print(f"Downloading and extracting assets...")
-
-    # Downloading zip file using urllib package.
-    urlretrieve(url, save_path)
-
-    try:
-        # Extracting zip file using the zipfile package.
-        with ZipFile(save_path) as z:
-            # Extract ZIP file contents in the same directory.
-            z.extractall(os.path.split(save_path)[0])
-
-        print("Done.")
-
-    except Exception as e:
-        print("\nInvalid file.", e)
-
 
 def load_model():
     global model
