0bb43601e66e731b7956a14a40e7ba789043729a
jblagana <jan.rhey.lagana@eee.upd.edu.ph>
2023-10-22 02:51:53 +0800

    Minor updates

--- main.py
+++ main.py
@@ -3,41 +3,40 @@ import sys
 import cv2 as cv
 from ultralytics import YOLO
 
+def process_video(s):
+    source = cv.VideoCapture(s)
+    while cv.waitKey(1) != 27:
+        has_frame, frame = source.read()
+        if not has_frame:
+            break
+
+        if isinstance(s, int):
+            frame = cv.flip(frame, 1)
+
+        results = model(frame)
+        annotated_frame = results[0].plot()
+        cv.imshow(WIN_NAME, annotated_frame)
+    source.release()
+    cv.destroyWindow(WIN_NAME)
+
+def process_image(s):
+    img = cv.imread(s)
+    results = model(img)
+    annotated_frame = results[0].plot()
+    cv.imshow(WIN_NAME, annotated_frame)
+    cv.waitKey(0)
+
 def detect_objects(s):
+
     cv.namedWindow(WIN_NAME, cv.WINDOW_NORMAL)
 
     if isinstance(s, int):
-        source = cv.VideoCapture(s)
-        while cv.waitKey(1) != 27:
-            has_frame, frame = source.read()
-            if not has_frame:
-                break
-            frame = cv.flip(frame, 1)
-            results = model(frame)
-            annotated_frame = results[0].plot()
-            cv.imshow(WIN_NAME, annotated_frame)
-        source.release()
-        cv.destroyWindow(WIN_NAME)
-
+        process_video(s)
     elif isinstance(s, str):
         if s.endswith('.mp4'):
-            source = cv.VideoCapture(s)
-            while cv.waitKey(1) != 27:
-                has_frame, frame = source.read()
-                if not has_frame:
-                    break
-                results = model(frame)
-                annotated_frame = results[0].plot()
-                cv.imshow(WIN_NAME, annotated_frame)
-            source.release()
-            cv.destroyWindow(WIN_NAME)
-
+            process_video(s)
         else:
-            img = cv.imread(s)
-            results = model(frame)
-            annotated_frame = results[0].plot()
-            cv.imshow(WIN_NAME, annotated_frame)
-            cv.waitKey(0)
+            process_image(s)
 
 
 if __name__ == "__main__":
