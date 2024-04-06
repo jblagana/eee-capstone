3f120f1f15e6bb4ce4df30264918bd844068e9db
ityno <ityano@up.edu.ph>
2024-04-06 23:20:06 +0800

    added new function detect_crowd_density

--- crowd_density/density_custom.py
+++ crowd_density/density_custom.py
@@ -3,6 +3,30 @@ import sys
 import cv2 as cv
 from ultralytics import YOLO
 
+def detect_crowd_density(boxes, frame):
+    segmented_area = 0
+    crowd_count = 0
+    crowd_density = 0
+
+    img = frame
+    frame_area = img.shape[0]*img.shape[1]
+    existing_boxes = []
+
+    for box in boxes:
+        x, y, w, h = box.tolist()
+        box_area = w * h
+        for existing_box in existing_boxes:
+            iou = calculate_iou((x, y, w, h), existing_box)
+            segmented_area += box_area * (1 - iou)  # Consider only non-overlapping area
+        existing_boxes.append((x, y, w, h))
+        crowd_count += 1
+
+    crowd_density = 100 * segmented_area / frame_area
+
+    #print(f"crowd density: {round(crowd_density, 2)}%")
+    return crowd_density, crowd_count
+
+
 def process_video(s):
     source = cv.VideoCapture(s)
     while cv.waitKey(1) != 27:
