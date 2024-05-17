e9e163803670ded306637d6a050f81a7e6a5adce
chaaiiiiy <clepilepsia@gmail.com>
2024-05-17 16:07:31 +0800

    added trt_integration profiling

---
+++ trt_integration/profiling/display.py
@@ -0,0 +1,43 @@
+# Displaying fps
+import matplotlib.pyplot as plt
+import csv
+
+parsed_data = []
+csv_file = "trt_integration/profiling/fps_log.csv"
+with open(csv_file, 'r', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    next(csv_reader)  # Skip header row
+    for row in csv_reader:
+        filename, frame_num, fps = row
+        frame_num = int(frame_num)
+        fps = float(fps)
+        parsed_data.append((filename, frame_num, fps))
+
+# Plot FPS data
+plt.figure()
+for filename, frame_num, fps in parsed_data:
+    plt.scatter(frame_num, fps, label=filename, marker='.')
+
+plt.xlabel('Frame Number')
+plt.ylabel('FPS')
+plt.title('FPS of Video Files')
+# plt.legend()
+plt.show()
+
+
+# Displaying block statistics
+import os
+import subprocess
+
+def visualize_profiles(profiling_folder):
+    profiles = os.listdir(profiling_folder)
+    for profile in profiles:
+        if profile.endswith(".prof"):
+            subprocess.Popen(["snakeviz", os.path.join(profiling_folder, profile)])
+
+if __name__ == "__main__":
+    profiling_folder = "trt_integration/profiling"
+    visualize_profiles(profiling_folder)
+
+
+
