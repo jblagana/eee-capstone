8b9677569ae0088ac4e7d10fd10f68c606e859eb
ityno <ityano@up.edu.ph>
2024-05-26 01:52:45 +0800

    parse arg for skipping frame

--- integration/main_skipping.py
+++ integration/main_skipping.py
@@ -185,7 +185,8 @@ def infer(input_sequence):
 
 def process_video(source, filename):
     #Global variables 
-    global yolo_path, bytetrack_path, max_age, persist_yolo
+    global yolo_path, bytetrack_path, max_age
+    global skip
     global frame_width, frame_height
     global font_scale, thickness, position, x_text, y_text, WIN_NAME
     global display_vid, save_vid, output_path
@@ -248,7 +249,7 @@ def process_video(source, filename):
 
         if frame_num % skip == 0:
             # Perform detection & tracking on frame
-            results = model.track(frame, persist=persist_yolo, verbose=False, tracker=bytetrack_path)
+            results = model.track(frame, persist=True, verbose=False, tracker=bytetrack_path)
             if results[0].boxes.id is not None:
                 boxes = results[0].boxes.xywh.cpu() 
                 track_ids = results[0].boxes.id.int().cpu().tolist()
@@ -376,12 +377,6 @@ def parse_args():
         help="Path of YOLO model."
     )
 
-    parser.add_argument(
-        "--persist-yolo",
-        default="store_false",
-        help="Displays YOLO inference if enabled."
-    )
-
     #Byetrack arguments
         #bytetrack yaml file
     parser.add_argument(
@@ -410,6 +405,13 @@ def parse_args():
         #Output video destination
         #Display window
         #Save log file
+    parser.skip_frames(
+        "--skip-frames",
+        type=int,
+        default=1,
+        help="Frames to be skipped during processing."
+    )
+
     parser.add_argument(
         "--device",
         type=str,
@@ -434,7 +436,7 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    skip = 5
+    skip = args.skip_frames
 
     #---------------YOLO & ByteTrack---------------#
     yolo_path = args.yolo_path
@@ -444,7 +446,6 @@ if __name__ == "__main__":
     except:
         print("Failed to load YOLO model.")
         sys.exit()
-    persist_yolo = args.persist_yolo
     
     bytetrack_path = args.bytetrack_path
     max_age = args.max_age
