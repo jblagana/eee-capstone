bc593b829d306572882195d4ddb38f64a2e90f27
chaaiiiiy <clepilepsia@gmail.com>
2024-05-30 19:22:02 +0800

    modified main_batch_skip for trt

--- trt_integration/main_batch_skip.py
+++ trt_integration/main_batch_skip.py
@@ -81,49 +81,77 @@ class YoloTRT():
         self.bindings = bindings
         self.det_output_length = host_outputs[0].shape[0]
 
-    def PreProcessImg(self, imgs):
+    # def PreProcessImg(self, imgs):
         
-        # Variables for batching
-        preprocessed_imgs = []
-        raw_imgs = []
-        orig_hs = []
-        orig_ws = []
+    #     # Variables for batching
+    #     preprocessed_imgs = []
+    #     raw_imgs = []
+    #     orig_hs = []
+    #     orig_ws = []
         
-        for img in imgs:
-            image_raw = img
-            h, w, c = image_raw.shape
-            image = cv.cvtColor(image_raw, cv.COLOR_BGR2RGB)
-            r_w = self.input_w / w
-            r_h = self.input_h / h
-            if r_h > r_w:
-                tw = self.input_w
-                th = int(r_w * h)
-                tx1 = tx2 = 0
-                ty1 = int((self.input_h - th) / 2)
-                ty2 = self.input_h - th - ty1
-            else:
-                tw = int(r_h * w)
-                th = self.input_h
-                tx1 = int((self.input_w - tw) / 2)
-                tx2 = self.input_w - tw - tx1
-                ty1 = ty2 = 0
-            image = cv.resize(image, (tw, th))
-            image = cv.copyMakeBorder(image, ty1, ty2, tx1, tx2, cv.BORDER_CONSTANT, None, (128, 128, 128))
-            image = image.astype(np.float32)
-            image /= 255.0
-            image = np.transpose(image, [2, 0, 1])
-            image = np.expand_dims(image, axis=0)
-            image = np.ascontiguousarray(image)
+    #     for img in imgs:
+    #         image_raw = img
+    #         h, w, c = image_raw.shape
+    #         image = cv.cvtColor(image_raw, cv.COLOR_BGR2RGB)
+    #         r_w = self.input_w / w
+    #         r_h = self.input_h / h
+    #         if r_h > r_w:
+    #             tw = self.input_w
+    #             th = int(r_w * h)
+    #             tx1 = tx2 = 0
+    #             ty1 = int((self.input_h - th) / 2)
+    #             ty2 = self.input_h - th - ty1
+    #         else:
+    #             tw = int(r_h * w)
+    #             th = self.input_h
+    #             tx1 = int((self.input_w - tw) / 2)
+    #             tx2 = self.input_w - tw - tx1
+    #             ty1 = ty2 = 0
+    #         image = cv.resize(image, (tw, th))
+    #         image = cv.copyMakeBorder(image, ty1, ty2, tx1, tx2, cv.BORDER_CONSTANT, None, (128, 128, 128))
+    #         image = image.astype(np.float32)
+    #         image /= 255.0
+    #         image = np.transpose(image, [2, 0, 1])
+    #         image = np.expand_dims(image, axis=0)
+    #         image = np.ascontiguousarray(image)
             
-            preprocessed_imgs.append(image)
-            raw_imgs.append(image_raw)
-            orig_hs.append(h)
-            orig_ws.append(w)
+    #         preprocessed_imgs.append(image)
+    #         raw_imgs.append(image_raw)
+    #         orig_hs.append(h)
+    #         orig_ws.append(w)
            
-        return np.vstack(preprocessed_imgs), raw_imgs, orig_hs, orig_ws 
-        # return image, image_raw, h, w
+    #     return np.vstack(preprocessed_imgs), raw_imgs, orig_hs, orig_ws 
+    #     # return image, image_raw, h, w
+
+    def PreProcessImg(self, img):
+        image_raw = img
+        h, w, c = image_raw.shape
+        image = cv.cvtColor(image_raw, cv.COLOR_BGR2RGB)
+        r_w = self.input_w / w
+        r_h = self.input_h / h
+        if r_h > r_w:
+            tw = self.input_w
+            th = int(r_w * h)
+            tx1 = tx2 = 0
+            ty1 = int((self.input_h - th) / 2)
+            ty2 = self.input_h - th - ty1
+        else:
+            tw = int(r_h * w)
+            th = self.input_h
+            tx1 = int((self.input_w - tw) / 2)
+            tx2 = self.input_w - tw - tx1
+            ty1 = ty2 = 0
+        image = cv.resize(image, (tw, th))
+        image = cv.copyMakeBorder(image, ty1, ty2, tx1, tx2, cv.BORDER_CONSTANT, None, (128, 128, 128))
+        image = image.astype(np.float32)
+        image /= 255.0
+        image = np.transpose(image, [2, 0, 1])
+        image = np.expand_dims(image, axis=0)
+        image = np.ascontiguousarray(image)
+        return image, image_raw, h, w
 
     def Inference(self, frames):
+
         imgs = frames
         host_inputs = self.host_inputs
         cuda_inputs = self.cuda_inputs
@@ -131,8 +159,26 @@ class YoloTRT():
         cuda_outputs = self.cuda_outputs
         bindings = self.bindings
 
-        input_images, raw_images, origin_hs, origin_ws = self.PreProcessImg(img)
-        np.copyto(host_inputs[0], input_images.ravel())
+        # Do image preprocess
+        batch_image_raw = []
+        batch_origin_h = []
+        batch_origin_w = []
+        batch_input_image = np.empty(shape=[self.batch_size, 3, self.input_h, self.input_w])  # Create an empty NumPy array with shape [batch_size, channels, height, width]
+        
+        # Iterate over imgs to preprocess each image in a batch
+        for i, image_raw in enumerate(imgs):
+            # Preprocess each image
+            input_image, image_raw, origin_h, origin_w = self.PreProcessImg(image_raw)
+
+            # Append the preprocessed image_raw, origin_h, and origin_w to their respective batches
+            batch_image_raw.append(image_raw)
+            batch_origin_h.append(origin_h)
+            batch_origin_w.append(origin_w)
+
+            # Copy the preprocessed input_image to the appropriate index in batch_input_image
+            np.copyto(batch_input_image[i], input_image)
+            
+        np.copyto(host_inputs[0], batch_input_image.ravel())
         stream = cuda.Stream()
         self.context = self.engine.create_execution_context()
         cuda.memcpy_htod_async(cuda_inputs[0], host_inputs[0], stream)
@@ -144,8 +190,10 @@ class YoloTRT():
         output = host_outputs[0]
         
         all_results = []
+
+        # Do postprocess
         for i in range(self.batch_size):
-            result_boxes, result_scores, result_classid = self.PostProcess(output[i * self.det_output_length: (i + 1) * self.det_output_length], origin_hs[i], origin_ws[i])            
+            result_boxes, result_scores, result_classid = self.PostProcess(output[i * self.det_output_length: (i + 1) * self.det_output_length], batch_origin_h[i], batch_origin_w[i])            
 
             det_res = []
             for j in range(len(result_boxes)):
@@ -159,6 +207,144 @@ class YoloTRT():
             all_results.append(det_res)
             
         return all_results, t2-t1
+
+    def PostProcess(self, output, origin_h, origin_w):
+        """
+        description: postprocess the prediction
+        param:
+            output:     A numpy likes [num_boxes,cx,cy,w,h,conf,cls_id, cx,cy,w,h,conf,cls_id, ...] 
+            origin_h:   height of original image
+            origin_w:   width of original image
+        return:
+            result_boxes: finally boxes, a boxes numpy, each row is a box [x1, y1, x2, y2]
+            result_scores: finally scores, a numpy, each element is the score correspoing to box
+            result_classid: finally classid, a numpy, each element is the classid correspoing to box
+        """
+        num_values_per_detection = self.DET_NUM + self.SEG_NUM + self.POSE_NUM
+        # Get the num of boxes detected
+        num = int(output[0])
+        # Reshape to a two dimensional ndarray
+        pred = np.reshape(output[1:], (-1, num_values_per_detection))[:num, :]
+        # Do nms
+        boxes = self.NonMaxSuppression(pred, origin_h, origin_w, conf_thres=self.CONF_THRESH, nms_thres=self.IOU_THRESHOLD)
+        result_boxes = boxes[:, :4] if len(boxes) else np.array([])
+        result_scores = boxes[:, 4] if len(boxes) else np.array([])
+        result_classid = boxes[:, 5] if len(boxes) else np.array([])
+        return result_boxes, result_scores, result_classid
+
+    def NonMaxSuppression(self, prediction, origin_h, origin_w, conf_thres=0.5, nms_thres=0.4):
+        """
+        description: Removes detections with lower object confidence score than 'conf_thres' and performs
+        Non-Maximum Suppression to further filter detections.
+        param:
+            prediction: detections, (x1, y1, x2, y2, conf, cls_id)
+            origin_h: original image height
+            origin_w: original image width
+            conf_thres: a confidence threshold to filter detections
+            nms_thres: a iou threshold to filter detections        
+        return:
+            boxes: output after nms with the shape (x1, y1, x2, y2, conf, cls_id)                
+        """
+        # Get the boxes that score > CONF_THRESH
+        boxes = prediction[prediction[:, 4] >= conf_thres]
+        # Trandform bbox from [center_x, center_y, w, h] to [x1, y1, x2, y2]
+        boxes[:, :4] = self.xywh2xyxy(origin_h, origin_w, boxes[:, :4])
+        # clip the coordinates
+        boxes[:, 0] = np.clip(boxes[:, 0], 0, origin_w - 1)
+        boxes[:, 2] = np.clip(boxes[:, 2], 0, origin_w - 1)
+        boxes[:, 1] = np.clip(boxes[:, 1], 0, origin_h - 1)
+        boxes[:, 3] = np.clip(boxes[:, 3], 0, origin_h - 1)
+        # Object confidence
+        confs = boxes[:, 4]
+        # Sort by the confs
+        boxes = boxes[np.argsort(-confs)]
+        # Perform non-maximum suppression
+        keep_boxes = []
+        while boxes.shape[0]:
+            large_overlap = self.bbox_iou(np.expand_dims(boxes[0, :4], 0), boxes[:, :4]) > nms_thres
+            label_match = boxes[0, -1] == boxes[:, -1]
+            # Indices of boxes with lower confidence scores, large IOUs and matching labels
+            invalid = large_overlap & label_match
+            keep_boxes += [boxes[0]]
+            boxes = boxes[~invalid]
+        boxes = np.stack(keep_boxes, 0) if len(keep_boxes) else np.array([])
+        return boxes
+    
+    def xywh2xyxy(self, origin_h, origin_w, x):
+        """
+        description:    Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+        param:
+            origin_h:   height of original image
+            origin_w:   width of original image
+            x:          A boxes numpy, each row is a box [center_x, center_y, w, h]
+        return:
+            y:          A boxes numpy, each row is a box [x1, y1, x2, y2]
+        """
+        y = np.zeros_like(x)
+        r_w = self.input_w / origin_w
+        r_h = self.input_h / origin_h
+        if r_h > r_w:
+            y[:, 0] = x[:, 0]
+            y[:, 2] = x[:, 2]
+            y[:, 1] = x[:, 1] - (self.input_h - r_w * origin_h) / 2
+            y[:, 3] = x[:, 3] - (self.input_h - r_w * origin_h) / 2
+            y /= r_w
+        else:
+            y[:, 0] = x[:, 0] - (self.input_w - r_h * origin_w) / 2
+            y[:, 2] = x[:, 2] - (self.input_w - r_h * origin_w) / 2
+            y[:, 1] = x[:, 1]
+            y[:, 3] = x[:, 3]
+            y /= r_h
+        return y
+    
+    def bbox_iou(self, box1, box2, x1y1x2y2=True):
+        """
+        description: compute the IoU of two bounding boxes
+        param:
+            box1: A box coordinate (can be (x1, y1, x2, y2) or (x, y, w, h))
+            box2: A box coordinate (can be (x1, y1, x2, y2) or (x, y, w, h))            
+            x1y1x2y2: select the coordinate format
+        return:
+            iou: computed iou
+        """
+        if not x1y1x2y2:
+            # Transform from center and width to exact coordinates
+            b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+            b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+            b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+            b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+        else:
+            # Get the coordinates of bounding boxes
+            b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+            b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+
+        # Get the coordinates of the intersection rectangle
+        inter_rect_x1 = np.maximum(b1_x1, b2_x1)
+        inter_rect_y1 = np.maximum(b1_y1, b2_y1)
+        inter_rect_x2 = np.minimum(b1_x2, b2_x2)
+        inter_rect_y2 = np.minimum(b1_y2, b2_y2)
+        # Intersection area
+        inter_area = (np.clip(inter_rect_x2 - inter_rect_x1 + 1, 0, None)
+                      * np.clip(inter_rect_y2 - inter_rect_y1 + 1, 0, None))
+        # Union Area
+        b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+        b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+        iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+        return iou
+    
+    def PlotBbox(self, x, img, color=None, label=None, line_thickness=None):
+        tl = (line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1)  # line/font thickness
+        # color = color or [random.randint(0, 255) for _ in range(3)]
+        c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+        cv.rectangle(img, c1, c2, color, thickness=tl, lineType=cv.LINE_AA)
+        if label:
+            tf = max(tl - 1, 1)  # font thickness
+            t_size = cv.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+            cv.rectangle(img, c1, c2, color, -1, cv.LINE_AA)  # filled
+            cv.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv.LINE_AA,)
 
 def parse_args():
     parser = ArgumentParser(
@@ -477,8 +663,10 @@ def process_video(source):
                         output = []   
                         boxes = []    
                         clss = []
+
                     elif not detections:
                         print("No detections found!")
+                        continue
                         
                     for detection in detections:        
                         box = detection["box"]
@@ -515,17 +703,19 @@ def process_video(source):
                     concealment_counts = concealment_module(clss)
                     #Loitering module
                     missed_detect, misses_cnt, dwell_time, loitering = loitering_module(frame, online_boxes, online_ids, clss, names, missed_detect, misses_cnt, dwell_time, max_age)
-
+                    print("frames cnt", frames_cnt[i])
                     module_result.append([frames_cnt[i], 
                             crowd_density, 
                             loitering, 
                             concealment_counts[3], concealment_counts[1], concealment_counts[2], concealment_counts[0]])
 
-        frame_batch = []
-        frames_cnt = []                
-        if not persist:
-            RBP = infer(module_result)
-            module_result = []
+            frame_batch = []
+            frames_cnt = [] 
+            print("module result: ", module_result)               
+            if (not persist) and (module_result != []):
+                print("inside RBP")
+                RBP = infer(module_result)
+                module_result = []
 
         # FPS Manual Calculation
         if fps_log:
@@ -609,6 +799,8 @@ if __name__ == "__main__":
     
     # Batching
     batch_size = model.batch_size
+    print("model batch size: ", model.batch_size)
+    # batch_size = 20
 
     #---------------ByteTrack---------------#
     args_bytetrack = argparse.Namespace()
